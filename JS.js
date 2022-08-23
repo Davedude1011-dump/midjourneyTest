449631350115d4b7ec5e4040221b9e039f533cc2
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-08-23 12:12:27 +0100

    Add files via upload

--- JS.js
+++ JS.js
@@ -3,11 +3,9 @@ function reveal() {
     document.querySelector(".video2").style.display = "block"
     document.querySelector(".video3").style.display = "block"
     document.querySelector(".video4").style.display = "block"
-}
-
-$("#playvideo").click(function(){
+    
     $(".video1")[0].src += "?autoplay=1";
     $(".video2")[0].src += "?autoplay=1";
     $(".video3")[0].src += "?autoplay=1";
     $(".video4")[0].src += "?autoplay=1";
-});
+}
